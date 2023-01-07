ff35ce19f5478f74b03c1112599d10d3689b1663
AristotelesDantas <arisdan@hotmail.com>
2023-01-07 14:49:06 -0300

    atualizado em 07/01/2023

--- app/src/main/java/com/example/agenda/MainActivity.java
+++ app/src/main/java/com/example/agenda/MainActivity.java
@@ -8,16 +8,18 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.view.View;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Lista de Alunos");
         List<String> alunos = new ArrayList<>(Arrays.asList("Ari", "Ze", "Joao"));
         ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
         listaDeAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos));
